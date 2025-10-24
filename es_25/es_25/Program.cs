217ddc5aebbdda1a263baa69379ebc47cfe70b1f
unknown <e.andru.3864@vallauri.net>
2025-10-24 12:54:47 +0200

    primo commit

---
+++ es_25/es_25/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "NN MRI GIOVNN";
+            char A = 'A';
+
+            cerca_lettera(s, A);
+        }
+        public static void cerca_lettera(string s, char A) 
+        {
+            bool a_is_present = false;
+            for(int i=0; i<s.Length; i++)
+            {
+                if (s[i] == A)
+                {
+                    Console.Write($"lettera A presente alla posizione {i.ToString()}\n");
+                    a_is_present = true;
+                }
+            }
+
+            if (!a_is_present)
+            {
+                Console.WriteLine("non ce nussun carattere A al interno della frase");
+            }
+        }
+
+    }
+}
